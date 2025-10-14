6374d8f8dd2f899d0544a202aa7a5bbeefd2ac8e
vanii0002 <0002sharbansal@gmail.com>
2025-10-14 18:53:52 +0530

    comlete google auth

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -115,10 +115,13 @@ export const home= async(req,res)=>{
       const {name,email}=req.body;   
          console.log('Google login data:', req.body);      
     let user= await User.findOne({email});
-
+let hassedPassword=await bcrypt.hash("Google Auth",10);
     if(!user){
-      user= await User.create({name,email, password: "GOOGLE_AUTH_USER"});
+      user= await User.create({name,email, password: hassedPassword});
     }   
+      
+       
+
         let token=generateToken({user:user._id});   
         res.cookie("token",token,
         { 
@@ -132,5 +135,30 @@ export const home= async(req,res)=>{
   } 
     catch(error){
         res.status(500).json({success:false,message:"User login failed",error:error.message});
+    }
+    };
+
+    export const githublogin= async(req,res)=>{
+      try{
+          const {name,email}=req.body;         
+    let user= await User.findOne({email});
+  let hassedPassword=await bcrypt.hash("Github Auth",10);   
+    if(!user){
+      user= await User.create({name,email, password: hassedPassword});
+    } 
+
+        let token=generateToken({user:user._id});   
+        res.cookie("token",token,
+        {     
+      httpOnly:true,
+      secure:false,
+      sameSite:"strict",
+      maxAge:24*60*60*1000,
+        }
+        );
+        res.status(200).json({success:true,message:"User logged in successfully",user});     
+  }
+    catch(error){
+        res.status(500).json({success:false,message:"User login failed",error:error.message});  
     }
     };
